7b27d89f87f5c850e828f3f6adaa1eb920c416ca
Miguel Kawakami <miguel.kawakami@noleak.io>
2024-05-16 19:52:24 -0300

    refactoring remove item function from Inventory

--- core/inventory.py
+++ core/inventory.py
@@ -24,7 +24,8 @@ class Inventory(BaseModel):
             print(f"O inventário está cheio. Não foi possível adicionar {item.name}")
 
     def rem(self, item: Item):
-        for i, _ in enumerate(self.items):
-            if _ == item:
-                del self.items[i]
-        print(f"Item '{item.name}' removido ao inventário")
+        if item in self.items:
+            self.items.remove(item)
+            print(f"Item '{item.name}' removido ao inventário")
+        else:
+            print("O item especificado não está no inventário")
